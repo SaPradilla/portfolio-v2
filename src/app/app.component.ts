49124d52e090acc5fbd832294f76a92b049437b2
SaPradi <a.garces@globant.com>
2024-12-15 22:10:29 -0500

    feat: add About and News components with styling and content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component, inject, OnInit } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ThemeService } from './core/services/theme.service';
+import { AboutComponent } from './pages/about/about.component';
+import { NewsComponent } from "./components/news/news.component";
+import { SkillsComponent } from './pages/skills/skills.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [
-        HomeComponent,
-        NavbarComponent,
-    ],
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    NewsComponent,
+    SkillsComponent,
+],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
---
+++ src/app/pages/skills/skills.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  standalone: true,
+  imports: [],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.scss'
+})
+export class SkillsComponent {
+  skills = {
+    frontend:[
+      {name:'Typescrpt',img:'assets/images/ts.png'},
+      {name:'Javascript',img:'assets/images/js.png'},
+      {name:'Angular',img:'assets/images/ng.png'},
+      {name:'React',img:'assets/images/react.png'},
+      {name:'Vue',img:'assets/images/vue.png'},
+      {name:'HTML',img:'assets/images/html.jpg'},
+      {name:'CSS',img:'assets/images/scss.png'},
+      {name:'Tailwind',img:'assets/images/tl.png'},
+      {name:'Pinia',img:'assets/images/pinia.png'},
+      {name:'Redux',img:'assets/images/redux.png'},
+      {name:'MUI',img:'assets/images/mui.png'},
+    ],
+    backend:[
+      { name: 'Node', img: 'assets/images/node.png' },
+      { name: 'PHP', img: 'assets/images/php.png' },
+      { name: 'Python', img: 'assets/images/py.png' },
+      { name: 'Laravel', img: 'assets/images/laravel.png' },
+      { name: 'Sequelize', img: 'assets/images/sequelize.png' },
+      { name: 'Socket.io', img: 'assets/images/socket.png' },
+      { name: 'Prisma', img: 'assets/images/prisma.png' },
+    ],
+    db:[
+      { name: 'MySQL', img: 'assets/images/mysql.png' },
+      { name: 'MongoDB', img: 'assets/images/mongo.png' },
+      { name: 'PostgreSQL', img: 'assets/images/postgres.png' },
+    ],
+    design:[
+      {name:'Figma',img:'assets/images/figma.png'},
+      {name:'Photoshop',img:'assets/images/photoshop.png'},
+      {name:'CorelDraw',img:'assets/images/coreldraw.png'},
+    ],
+    others:[
+      {name:'Git',img:'assets/images/git.png'},
+      {name:'Docker',img:'assets/images/docker.png'},
+      {name:'Linux',img:'assets/images/linux.png'},
+      {name:'Postman',img:'assets/images/postman.png'},
+    ]
+  }
+}
